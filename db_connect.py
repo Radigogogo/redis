d336568b7e0402f3f1b75a3ad940e9d268155adb
radi <radi@>
2021-10-27 18:44:45 +0800

    fold programs to function and they work

--- db_connect.py
+++ db_connect.py
@@ -1,27 +1,41 @@
 import pymssql
 import redis
 
-# tic DB connection
-conn_ticdb = pymssql.connect(
-    server='PC-RADI_MA',
-    user='Radi',
-    password='phison',
-    database='TIC_DB'
-)
+def tic_db_conn():
+    # tic DB connection
+    try:
+        conn_ticdb = pymssql.connect(
+            server='PC-RADI_MA',
+            user='Radi',
+            password='phison',
+            database='TIC_DB'
+        )
+        #tic DB select
+        cursor = conn_ticdb.cursor(as_dict=True)
+        cursor.execute('select top 10 * from [TIC_DB].[dbo].[Card_Info]')
+        print(cursor)
+        for row in cursor :
+            print(row)
+        #close tic db connection
+        conn_ticdb.close()
+    except Exception as e:
+        print(e)
 
-conn_redis_params = {
-    'host' : '127.0.0.1',
-    'port' : 6379,
-    'db' : 0
-}
-conn_redis = redis.StrictRedis(**conn_redis_params )
+def redis_conn():
+    try:
+        conn_redis_params = {
+            'host':  '127.0.0.1',
+            'port': 6379,
+            'db': 0
+        }
+        conn_redis = redis.StrictRedis(**conn_redis_params)
+        conn_redis.set('tt','123')
+        msg = conn_redis.get('tt')
+        print(msg)
 
-#tic DB select
-cursor = conn_ticdb.cursor(as_dict=True)
-cursor.execute('select top 10 * from [TIC_DB].[dbo].[Card_Info]')
-print(cursor)
-for row in cursor :
-    print(row)
+    except Exception as e:
+        print(e)
 
-#close tic db connection
-conn_ticdb.close()
+if __name__=='__main__':
+    tic_db_conn()
+    redis_conn()
