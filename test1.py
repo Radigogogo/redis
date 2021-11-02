7e47ae3e5f9005d4e0f835d80886e0a3c61bdac4
radi <radi@>
2021-11-02 20:56:11 +0800

    try test1

--- test1.py
+++ test1.py
@@ -15,27 +15,38 @@ def tic_db_conn():
         cursor = conn_ticdb.cursor(as_dict=True)
         #cursor.execute('select top 10 * from [TIC_DB].[dbo].[Card_Info]')
         query = """
-        SELECT Running_StartTime, Test_End_Time,
-        (SELECT Name1 FROM dbo.VRs_View_Code_Describe WHERE System = 'VRS' AND Type = '101' AND Code = TK.Test_Result) AS Test_Result,
-        (SELECT Name1 FROM dbo.VRs_View_Code_Describe WHERE System = 'VRS' AND Type = '100' AND Code = TK.Test_Status) AS Test_Status,
-        TK.TL_ID,
-        Tool_Name,
-        Tool_Version,
-        PT.Test_Result AS Pattern_Result,
-        PT.Pattern_Name,
-        PT.Test_Duration AS Pattern_Duration,
-        TK.Tester_ID,
-        TK.Test_Status
-        FROM dbo.VRs_View_Base_Task AS TK
-        LEFT JOIN dbo.Tool_List TL ON TK.TL_ID = TL.TL_ID
-        LEFT JOIN dbo.Pattern_List PT ON TK.TK_ID = PT.TK_ID
+                    
+            with 
+            tt as(
+            SELECT TOP(10)
+            TK.TL_ID,
+            ISNULL(TK.Tester_ID, 0) as Tester_ID,
+            TK.Test_Status,
+            ISNULL(Running_StartTime,0) as Running_StartTime, 
+            Test_End_Time,
+            (SELECT Name1 FROM dbo.VRs_View_Code_Describe WHERE System = 'VRS' AND Type = '101' AND Code = TK.Test_Result) AS Test_Result,
+            (SELECT Name1 FROM dbo.VRs_View_Code_Describe WHERE System = 'VRS' AND Type = '100' AND Code = TK.Test_Status) AS Test_Status_2,
+            Tool_Name,
+            Tool_Version,
+            ISNULL(PT.Test_Result,0) AS Pattern_Result,
+            ISNULL(PT.Pattern_Name, 0) as Pattern_Name,
+            ISNULL(PT.Test_Duration,0) AS Pattern_Duration
+            FROM dbo.VRs_View_Base_Task AS TK
+            LEFT JOIN dbo.Tool_List TL ON TK.TL_ID = TL.TL_ID
+            LEFT JOIN dbo.Pattern_List PT ON TK.TK_ID = PT.TK_ID
+            )
+            
+            Select * 
+            from  tt 
+            for json auto
         """
         cursor.execute(query)
-        print(cursor)
+        #print(cursor)
         mkey = None
         mval = None
         for row in cursor:
-            print(row[0])
+            print(row)
+
 
 
 
